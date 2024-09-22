5a5f323dd753a9712b759dc973f8b9c77df69df1
victormartindz <victor.martin.dz@gmail.com>
2024-09-22 10:44:19 +0200

    added Fraction Class related Tests to User Class Test #22

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/UserTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/UserTest.java
@@ -1,11 +1,15 @@
 package es.upm.miw.iwvg_devops.rest.code;
 
+import es.upm.miw.iwvg_devops.code.Fraction;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import es.upm.miw.iwvg_devops.code.User;
+import es.upm.miw.iwvg_devops.code.Fraction;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class UserTest {
 
@@ -20,6 +24,18 @@ public class UserTest {
     public void testDefaultConstructor() {
         User user = new User();
         assertTrue((user.getFractions().isEmpty()));
+    }
+
+    @Test
+    public void testParametrizedConstructor() {
+        List<Fraction> fractionList = new ArrayList<>();
+        fractionList.add(new Fraction(1, 2));
+        fractionList.add(new Fraction(3, 10));
+        User user = new User("2", "Orson", "Scott Card", fractionList);
+        assertEquals("2", user.getId());
+        assertEquals("Orson", user.getName());
+        assertEquals("Scott Card", user.getFamilyName());
+        assertEquals(fractionList, user.getFractions());
     }
 
     @Test
@@ -50,6 +66,22 @@ public class UserTest {
     }
 
     @Test
+    public void testGetFractions() {
+        user.addFraction(new Fraction(1, 3));
+        Fraction fraction = user.getFractions().get(0);
+        assertEquals(1, fraction.getNumerator());
+        assertEquals(3, fraction.getDenominator());
+    }
+
+    @Test
+    public void testSetFractions() {
+        List<Fraction> fractionList = new ArrayList<>();
+        fractionList.add(new Fraction(2, 5));
+        user.setFractions(fractionList);
+        assertEquals(fractionList, user.getFractions());
+    }
+
+    @Test
     public void testFullName() {
 
         assertEquals("Stanislaw Lem", user.fullName());
@@ -58,5 +90,16 @@ public class UserTest {
     @Test
     public void TestInitials() {
         assertEquals("S.",user.initials());
+    }
+
+    @Test
+    public void testToString() {
+        List<Fraction> fractions = new ArrayList<>();
+        fractions.add(new Fraction(1, 3));
+        fractions.add(new Fraction(2, 5));
+        user.setFractions(fractions);
+
+        String expected = "User{id='1', name='Stanislaw', familyName='Lem', fractions=[Fraction{numerator=1, denominator=3}, Fraction{numerator=2, denominator=5}]}";
+        assertEquals(expected, user.toString());
     }
 }
